8174f91eae030eacec5556e702398c16ea67de2e
Colin Stevens <mail@colinjstevens.com>
2018-10-23 21:32:15 -0500

    Basic UI built

---
+++ ConversionCalculator/ConversionCalculator/ConverterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConverterViewController.swift
+//  ConversionCalculator
+//
+//  Created by Stevens, Colin (MU-Student) on 10/23/18.
+//  Copyright © 2018 Colin Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
